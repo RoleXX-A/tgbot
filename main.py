84068f9c810e8bad38a8b67af9d9d6caa13af75b
RoleXX-A <133496601+RoleXX-A@users.noreply.github.com>
2023-08-20 19:59:23 +0000

    1

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ bot = telebot.TeleBot(secret_key)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-  bot.send_message(message.chat.id, 'Hello Player' )
+  bot.send_message(message.chat.id, 'Hello' )
 
 bot.infinity_polling()
